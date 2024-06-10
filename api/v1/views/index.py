4cbdf94b641488e02abc11415d14cb7a7810280e
sofoinasgd <zephaniahsofa@gmail.com>
2024-06-10 11:19:33 +0000

    added task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""index module"""
+
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+from models import storage
+
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
+    return jsonify({"status": "OK"})
